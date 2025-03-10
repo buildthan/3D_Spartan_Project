ceb583b0d3ad7d15ea2507653e92485bc94173f6
buildthan <a9175226@gmail.com>
2025-03-10 21:42:37 +0900

    [ Feat ] HP BAR 기능 추가

---
+++ Assets/Scripts/PlayerConditions.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerConditions : MonoBehaviour
+{
+    public Image HealthUI;
+
+    public float curHealth;
+    public float maxHealth;
+    public float startHealth;
+    public float passiveHealth;
+
+    public void Start()
+    {
+        curHealth = startHealth;
+    }
+
+    public void FixedUpdate()
+    {
+        if (curHealth > maxHealth)
+        {
+            curHealth = maxHealth;
+        }
+
+        if(curHealth < 0)
+        {
+            curHealth = 0;
+        }
+
+        if( curHealth < maxHealth) //패시브 체력 회복량
+        {
+            curHealth += passiveHealth * Time.deltaTime;
+        }
+
+
+        HealthUI.fillAmount = curHealth / maxHealth;
+
+    }
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -22,6 +22,7 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
         rigidbody = GetComponent<Rigidbody>();
     }
 
